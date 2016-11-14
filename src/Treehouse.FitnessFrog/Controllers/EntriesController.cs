a3e3f100e77609bf0faac8ea3f24e5f077cbe0a5
sansait <sansait@SANSAIT10>
2016-11-14 10:11:42 -0600

    Added EntiresController AddPOST action method

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using Treehouse.FitnessFrog.Data;
 using Treehouse.FitnessFrog.Models;
@@ -41,6 +39,14 @@ namespace Treehouse.FitnessFrog.Controllers
 
         public ActionResult Add()
         {
+
+            return View();
+        }
+
+        [ActionName("Add"), HttpPost]
+        public ActionResult AddPost()
+        {
+
             return View();
         }
 
